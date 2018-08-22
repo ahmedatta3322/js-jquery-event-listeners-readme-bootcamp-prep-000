5aaf9fa7530a6748e684f1b8572403140e0fabb9
ahmed atta <ahmedatta3322@gmail.com>
2018-08-22 19:21:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@ function getIt(){
 }
 function frameIt(){
   img1 = $('img')
-  return $('img').on('load' , img1.css = "tasty")
+  return $(img1).on('load' , img1.css = "tasty")
 } 
 $(document).ready(function(){
 
