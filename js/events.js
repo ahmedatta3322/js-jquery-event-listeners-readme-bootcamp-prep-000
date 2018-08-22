c35bdebd5907f12ee5a2cce26e31f617f7a9c4af
ahmed atta <ahmedatta3322@gmail.com>
2018-08-22 19:19:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@ function getIt(){
   return $('p').on('click' , window.alert('hey'))
 }
 function frameIt(){
-  return $('img').on('load' ,  = "tasty")
+  return $('img').on('load' , css = "tasty")
 } 
 $(document).ready(function(){
 
